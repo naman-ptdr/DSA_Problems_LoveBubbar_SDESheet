275e561a0bcb8534752309f869bbc727b3bec3a8
Naman Patidar <namanpatidar5555@gmail.com>
2024-12-05 13:57:59 +0530

    constructor and distructor

--- lec123_OOPs2.cpp
+++ lec123_OOPs2.cpp
@@ -2,15 +2,61 @@
 using namespace std;
 
 class Animal{
+    // state or properties
+    private:
+    int weight = 20;
     public:
     int age;
     string name;
 
+    // constructor   
+    // default constructor
+    Animal(){
+        this->weight = 0;
+        this->age = 0;
+        this->name = "";
+        cout<<"Constructor Called"<<endl;
+    }
+
+    // Parameterised Constructor
+    Animal(int age){
+        this->age = age;
+        cout<<"Parameterised Constructor Called"<<endl;
+    }
+
+    Animal(int weight, int age, string name){
+        this->weight = weight;
+        this->age = age;
+        this->name = name;
+        cout<<"Parameterised Constructor Two Called"<<endl;
+    }
+
+    // Copy Constructor
+    Animal(Animal &obj){
+        this->age = obj.age;
+        this->weight = obj.weight;
+        this->name = obj.name;
+        cout<<"Copy Constructor Called"<<endl;
+    }
+    // behavior
     void sleep(){
         cout<<"Sleep"<<endl;
     }
     void eat(){
         cout<<"Eat"<<endl;
+    }
+    void setWeight(int weight){
+        this->weight = weight;
+    }
+    int getWeight(){
+        return weight;
+    }
+
+    void print(){
+        cout<<this->age<<" "<<this->weight<<" "<<this->name<<endl;
+    }
+    ~Animal(){
+        cout<<"Inside distructor"<< endl;
     }
 };
 int main(){
@@ -24,5 +70,31 @@ int main(){
 
     suresh->eat();
     suresh->sleep();
+
+
+    // parameterised Constructor calling
+    // Animal* a = new Animal(100);
+    // Animal* b = new Animal(20, 30, "Kutta");
+    // Animal c = *b;     //c(*b)
+
+    // Animal a;
+    // a.age = 20;
+    // a.setWeight(101);
+    // a.name = "babbar";
+
+    // Animal b = a;
+    // a.print();
+    // b.print();
+
+    cout<<"a obj creation"<<endl;
+    Animal a;
+    a.age = 5;
+    // default called distructor for static memory allocation
+    cout<<"b obj creation"<<endl;
+    Animal* b = new Animal();
+    b->age = 12;
+    // manually called distructor for dynamic allocation
+    delete b;
+
 return 0;
 }
