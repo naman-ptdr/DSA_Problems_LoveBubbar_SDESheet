8c014c2184e57b943be735e64a855a9f9bf3e797
Naman Patidar <namanpatidar5555@gmail.com>
2025-02-06 12:19:44 +0530

    Dynamic memory allocation 2D Array construction

---
+++ lec125_DynamicMemory.cpp
@@ -0,0 +1,25 @@
+// 2d array
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int row = 5;
+    int col = 3;
+    int **arr = new int*[row];
+    for(int i=0; i<row; i++){
+        arr[i] = new int [col];
+    }
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            cin>>arr[i][j];
+        }
+    }
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+return 0;
+}
